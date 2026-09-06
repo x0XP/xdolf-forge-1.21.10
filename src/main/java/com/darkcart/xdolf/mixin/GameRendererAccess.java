2464da67065d8bb4f4c60f84070e6af46cc034a9
x0XP <3501503+x0XP@users.noreply.github.com>
2026-09-06 10:55:23 +0100

    Expose view bob transform for stable tracers

--- src/main/java/com/darkcart/xdolf/mixin/GameRendererAccess.java
+++ src/main/java/com/darkcart/xdolf/mixin/GameRendererAccess.java
@@ -1,5 +1,6 @@
 package com.darkcart.xdolf.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,4 +9,5 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(GameRenderer.class)
 public interface GameRendererAccess {
     @Invoker("getFov") float xdolf$fov(Camera camera, float partialTick, boolean changingFov);
+    @Invoker("bobView") void xdolf$bobView(PoseStack poseStack, float partialTick);
 }
